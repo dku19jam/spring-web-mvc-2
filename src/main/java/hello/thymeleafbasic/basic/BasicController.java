463c7ebb7c1cbde60aaf7d887310b56b4f252259
Choi jaemin <panzzang518@naver.com>
2022-07-25 09:02:37 +0900

    리터럴 대체

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -68,6 +68,12 @@ public class BasicController {
         model.addAttribute("param2", "data2");
         return "basic/link";
     }
+
+    @GetMapping
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
     @Data
     static class User {
         private String username;
